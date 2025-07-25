65639c955e7fb081f18a55ff6a03fdfe7dfa2e3a
Witek <witold.zzak@gmail.com>
2025-07-26 01:40:29 +0200

    Added short description

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
 with open("README.md", "r") as f:
-    description = f.read()
+    text = f.read()
 
 setup(
     name='keyslide',
-    version='0.5',
+    version='0.7',
     entry_points={
         "console_scripts": [
             "keyslide.encrypt = keyslide:cli_encrypt",
             "keyslide.decrypt = keyslide:cli_decrypt"
         ],
     },
-    long_description=description,
+    long_description=text,
+    description="Encrypt text by shifting keys on the QWERTY keyboard.",
     long_description_content_type="text/markdown"
 )
